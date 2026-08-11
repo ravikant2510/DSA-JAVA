94774292bcc0d483d8b264b66ae29500b6bf9375
Ravi Kant <purbeyravi123@gmail.com>
2026-08-11 11:49:06 +0530

    Time: 1 ms (93.72%) | Memory: 43.8 MB (88.14%) - LeetSync

--- 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -1,18 +1,41 @@
 class Solution {
     public int missingInteger(int[] nums) {
-        int sum = nums[0];
+        // int sum = nums[0];
+        // int n = nums.length;
+        // for(int i = 1;i<n;i++) {
+        //     if(nums[i]==nums[i-1]+1) {
+        //         sum += nums[i];
+        //     } else {
+        //         break;
+        //     }
+        // }
+        // Arrays.sort(nums);
+        // for(int i =0;i<n;i++) {
+        //     if(sum == nums[i]) {
+        //         sum++;
+        //     }
+        // }
+        // return sum;
+
         int n = nums.length;
+        boolean[] check = new boolean[52];
+        int sum =nums[0];
+        boolean seq = true;
+        check[nums[0]] = true;
         for(int i = 1;i<n;i++) {
-            if(nums[i]==nums[i-1]+1) {
+            if(seq && nums[i] == nums[i-1]+1) {
                 sum += nums[i];
             } else {
-                break;
+                seq = false;
+                if(sum > 50){
+                    return sum;
+                }
             }
+            check[nums[i]] = true;
         }
-        Arrays.sort(nums);
-        for(int i =0;i<n;i++) {
-            if(sum == nums[i]) {
-                sum++;
+        for(int i = sum;i<52;i++) {
+            if(!check[i]) {
+                return i;
             }
         }
         return sum;
