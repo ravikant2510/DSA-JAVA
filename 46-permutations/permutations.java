626ea292800dc2db1b7d71c40b0b68f5bb690e48
Ravi Kant <purbeyravi123@gmail.com>
2026-08-23 13:22:38 +0530

    Time: 2 ms (33.93%) | Memory: 45.2 MB (90.10%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        backTrack(list,temp,nums,0);
+        return list;
+    }
+    private void backTrack(List<List<Integer>> list, List<Integer> temp , int[] nums,int start) {
+        if(temp.size() == nums.length) {
+            list.add(new ArrayList<>(temp));
+        } else {
+            for(int i=0;i<nums.length;i++) {
+                if(temp.contains(nums[i])) continue;
+                temp.add(nums[i]);
+                backTrack(list,temp,nums,i+1);
+                temp.remove(temp.size()-1);
+            }
+        }
+    }
+}
