1e94fa9fd6dd818c3875be59880b51e851afd36f
Ravi Kant <purbeyravi123@gmail.com>
2026-08-23 13:27:04 +0530

    Time: 2 ms (33.93%) | Memory: 45.8 MB (15.67%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -2,17 +2,17 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
-        backTrack(list,temp,nums,0);
+        backTrack(list,temp,nums);
         return list;
     }
-    private void backTrack(List<List<Integer>> list, List<Integer> temp , int[] nums,int start) {
+    private void backTrack(List<List<Integer>> list, List<Integer> temp , int[] nums) {
         if(temp.size() == nums.length) {
             list.add(new ArrayList<>(temp));
         } else {
             for(int i=0;i<nums.length;i++) {
                 if(temp.contains(nums[i])) continue;
                 temp.add(nums[i]);
-                backTrack(list,temp,nums,i+1);
+                backTrack(list,temp,nums);
                 temp.remove(temp.size()-1);
             }
         }
